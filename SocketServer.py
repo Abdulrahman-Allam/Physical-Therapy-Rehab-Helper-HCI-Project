3e43999283e5b0f14cc71eb4ee2f2c00f57b1693
Abdulrahman-Allam <91520392+Abdulrahman-Allam@users.noreply.github.com>
2024-11-04 23:12:17 +0200

    Update SocketServer.py and Fixing some issues

--- SocketServer.py
+++ SocketServer.py
@@ -9,12 +9,14 @@ import math
 import mediapipe as mp 
 import cv2 
 from dollarpy import Recognizer, Template, Point
+import platform
+
+maclist = ['B0:E4:5C:37:97:1F', '6F:F2:1E:3F:B2:BB', 'A0:D0:5B:27:31:17']
 
 # Define mediapipe holistics
-mp_drawing = mp.solutions.drawing_utils # Drawing helpers
+mp_drawing = mp.solutions.drawing_utils  # Drawing helpers
 mp_drawing_styles = mp.solutions.drawing_styles
-
-mp_holistic = mp.solutions.holistic # Mediapipe Solutions
+mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 
 # Define server IP and port
 host_ip = '0.0.0.0'  # Listen on all available interfaces
@@ -28,67 +30,45 @@ server_socket.listen(1)  # Listen for 1 client connection at a time
 client_socket = None
 is_client_connected = False
 
-def getPoints(videoURL,label):
-    cap = cv2.VideoCapture(videoURL)#web cam =0 , else enter filename
-    # Initiate holistic model
+def getPoints(videoURL, label):
+    cap = cv2.VideoCapture(videoURL)  # webcam = 0, else enter filename
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         points = []
-        left_shoulder=[]#
-        right_shoulder=[] #
-        left_elbos=[]#
-        right_elbos=[]#
-        nose=[]#
-        left_wirst=[]#
-        right_wrist=[]#
-        left_hip=[]#
-        right_hip=[]#     
+        left_shoulder, right_shoulder = [], []
+        left_elbos, right_elbos = [], []
+        nose, left_wirst, right_wrist = [], [], []
+        left_hip, right_hip = [], []     
+
         while cap.isOpened():
             ret, frame = cap.read()
-
-            # Recolor Feed
-            if ret==True:
-
+            if ret:
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                image.flags.writeable = False        
-
-                # Make Detections
+                image.flags.writeable = False
                 results = holistic.process(image)
-                # print(results.face_landmarks)
 
-
-                # Recolor image back to BGR for rendering
                 image.flags.writeable = True   
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-                # Drawing on Frame (You can remove it)
-                # 2. Right hand
                 mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-
-                # 3. Left Hand
                 mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-
-                # 4. Pose Detections
-                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-                # Export coordinates
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                                          landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
                 try:
-
-                    # add points of wrist , elbow and shoulder
-                    left_shoulder.append(Point(results.pose_landmarks.landmark[11].x,results.pose_landmarks.landmark[11].y,1))
-                    right_shoulder.append(Point(results.pose_landmarks.landmark[12].x,results.pose_landmarks.landmark[12].y,2))
-                    left_elbos.append(Point(results.pose_landmarks.landmark[13].x,results.pose_landmarks.landmark[13].y,3))
-                    right_elbos.append(Point(results.pose_landmarks.landmark[14].x,results.pose_landmarks.landmark[14].y,4))
-                    left_wirst.append(Point(results.pose_landmarks.landmark[15].x,results.pose_landmarks.landmark[15].y,5))
-                    right_wrist.append(Point(results.pose_landmarks.landmark[16].x,results.pose_landmarks.landmark[16].y,6))
-                    left_hip.append(Point(results.pose_landmarks.landmark[23].x,results.pose_landmarks.landmark[23].y,7))
-                    right_hip.append(Point(results.pose_landmarks.landmark[24].x,results.pose_landmarks.landmark[24].y,8))
-                    nose.append(Point(results.pose_landmarks.landmark[0].x,results.pose_landmarks.landmark[0].y,9))
-
+                    left_shoulder.append(Point(results.pose_landmarks.landmark[11].x, results.pose_landmarks.landmark[11].y, 1))
+                    right_shoulder.append(Point(results.pose_landmarks.landmark[12].x, results.pose_landmarks.landmark[12].y, 2))
+                    left_elbos.append(Point(results.pose_landmarks.landmark[13].x, results.pose_landmarks.landmark[13].y, 3))
+                    right_elbos.append(Point(results.pose_landmarks.landmark[14].x, results.pose_landmarks.landmark[14].y, 4))
+                    left_wirst.append(Point(results.pose_landmarks.landmark[15].x, results.pose_landmarks.landmark[15].y, 5))
+                    right_wrist.append(Point(results.pose_landmarks.landmark[16].x, results.pose_landmarks.landmark[16].y, 6))
+                    left_hip.append(Point(results.pose_landmarks.landmark[23].x, results.pose_landmarks.landmark[23].y, 7))
+                    right_hip.append(Point(results.pose_landmarks.landmark[24].x, results.pose_landmarks.landmark[24].y, 8))
+                    nose.append(Point(results.pose_landmarks.landmark[0].x, results.pose_landmarks.landmark[0].y, 9))
                 except:
                     pass
 
                 cv2.imshow(label, image)
-            else :
+            else:
                 cap.release()
                 cv2.destroyAllWindows()
                 cv2.waitKey(100)
@@ -100,9 +80,7 @@ def getPoints(videoURL,label):
                 cv2.waitKey(100)
                 break
 
-    cap.release()
-    cv2.destroyAllWindows()
-    points = left_shoulder+right_shoulder+left_elbos+right_elbos+left_wirst+right_wrist+left_hip+right_hip+nose
+    points = left_shoulder + right_shoulder + left_elbos + right_elbos + left_wirst + right_wrist + left_hip + right_hip + nose
     print(label)
     return points
 
@@ -111,17 +89,15 @@ def socket_server():
     print(f"Server listening on {host_ip}:{port}...")
 
     try:
-        # Wait for a client to connect
         client_socket, client_address = server_socket.accept()
         print(f"Connected to client at {client_address}")
-        is_client_connected = True  # Mark client as connected
+        is_client_connected = True
 
         while is_client_connected:
-            time.sleep(5)  # Keep the connection open and sleep
+            time.sleep(5)
     except Exception as e:
         print(f"An error occurred in socket server: {e}")
     finally:
-        # Clean up the connection
         if client_socket:
             client_socket.close()
         server_socket.close()
@@ -129,45 +105,43 @@ def socket_server():
         print("Socket server closed")
 
 async def scan_bluetooth_devices():
-    # Wait until a client is connected before scanning
     while not is_client_connected:
         print("Waiting for client connection to start Bluetooth scan...")
         await asyncio.sleep(1)
 
     print("Scanning for Bluetooth devices...")
     scan_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
+
     try:
         devices = await BleakScanner.discover()
-        
         print(f"\nScan completed at {scan_time}")
         print(f"Found {len(devices)} devices:\n")
-        
+
         for device in devices:
             device_info = f"Device Name: {device.name or 'Unknown'}, MAC Address: {device.address}"
             print(device_info)
-            # Send the device info to the client
-            #send_to_client(device_info)
-
-            # Load the recognizer model from the file
-            with open('recognizer_model.pkl', 'rb') as file:
-                recognizer = pickle.load(file)
-
-            #TEST
-            vid = "../dataset/CatCow-Wrong-4.mp4" # use "/videos/"" + username
-            points = getPoints(vid,"Unknown") # keep as unknown
-
-            start = time.time()
-            result = recognizer.recognize(points)
-            end = time.time()
-            print(result[0])
-            print("time taken to classify:"+ str(end-start))
-
-            score = math.floor(result[1] * 1000)
-
             if device.address == 'A0:D0:5B:27:31:17':
-                send_to_client(f"{device.address, score}")
-            
+                send_to_client(f"{device.address}, ")
+                break
+
+            elif device.address in maclist:
+                with open('recognizer_model.pkl', 'rb') as file:
+                    recognizer = pickle.load(file)
+
+                vid = "videos/CatCow-Wrong-5.mp4"
+                points = getPoints(vid, "Unknown")
+
+                start = time.time()
+                result = recognizer.recognize(points)
+                end = time.time()
+                print(result[0])
+                print("Time taken to classify: " + str(end - start))
+
+                score = math.floor(result[1] * 1000)
+                print(f"this is your score: {score}")
+                send_to_client(f"{device.address},{score}")
+                break
+
     except Exception as e:
         print(f"An error occurred in Bluetooth scan: {str(e)}")
 
@@ -181,14 +155,17 @@ def send_to_client(message):
     except Exception as e:
         print(f"Error sending to client: {e}")
 
-async def main():
-    print("Starting Bluetooth scan (after client connection)...")
-    await scan_bluetooth_devices()
+def run_scan_in_loop():
+    if platform.system() == "Windows":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(scan_bluetooth_devices())
+    loop.close()
 
 if __name__ == "__main__":
-    # Start the socket server in a separate thread
     server_thread = threading.Thread(target=socket_server)
     server_thread.start()
 
-    # Run the async function for scanning Bluetooth devices
-    asyncio.run(main())
+    scan_thread = threading.Thread(target=run_scan_in_loop)
+    scan_thread.start()
